c8b3e810845543c7c487b955a6b4de85a3049ef0
Ehb <ehabaftab@hotmail.com>
2021-12-01 12:18:38 +0500

    fix

--- day1.py
+++ day1.py
@@ -1,13 +1,13 @@
 f = open("input_files/day1.in")
-previous = -2
 count = 0
+previous = 0
 entries = f.readlines()
 i = 0
 while len(entries) - i >= 3:
     e1 = int(entries[i])
     e2 = int(entries[i+1])
     e3 = int(entries[i+2])
-    if(e1+e2+e3 > previous):
+    if(e1+e2+e3 > previous and i > 0):
         count += 1
     previous = e1+e2+e3
     i += 1
